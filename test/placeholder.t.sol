a532c48c52c6820f1378e709c25875af690f42a9
coopergau <cooperg122@gmail.com>
2024-09-26 13:13:20 -0600

    Added placeholder test

---
+++ test/placeholder.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import "forge-std/Test.sol";
+
+contract PlaceholderTest is Test {
+    function testPlaceholder() public pure {
+        assert(true);
+    }
+}
