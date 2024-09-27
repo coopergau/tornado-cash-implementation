13d4dd3e88881c13da6c6d6689bdb574770d43db
coopergau <cooperg122@gmail.com>
2024-09-26 22:15:58 -0600

    Added withdraw circom circuit corresponding verifier smart contract

---
+++ circuits/withdraw.circom
@@ -0,0 +1,56 @@
+pragma circom 2.1.9;
+
+include "../node_modules/circomlib/circuits/pedersen.circom";
+include "../node_modules/circomlib/circuits/mimcsponge.circom";
+
+template Withdraw(levels) {
+    // Public inputs
+    signal input _root;
+    signal input _nullifierHash;
+    signal input _address;
+
+    // Private inputs
+    signal input secret[256];
+    signal input nullifier[256];
+    signal input siblingNodes[levels];
+    signal input hashDirections[levels];
+    
+    // Verify the nullifier hash
+    component nullHasher = Pedersen(256);
+    nullHasher.in <== nullifier;    
+    signal nullHashed <== nullHasher.out[0];
+    _nullifierHash === nullHashed;
+
+    // Verify the merkle root
+    signal concat[512];
+    for (var i = 0; i < 256; i++) {
+        concat[i] <== nullifier[i];
+        concat[256 + i] <== nullifier[i];
+    }
+    component commitmentHasher = Pedersen(512);
+    commitmentHasher.in <== concat;
+    signal rootPath[levels + 1];
+    rootPath[0] <== commitmentHasher.out[0];
+
+    signal left[levels];
+    signal right[levels];
+    component mimcHashers[levels];
+
+    for (var i = 0; i < levels; i++) {
+        mimcHashers[i] = MiMCSponge(2, 91, 1);
+        mimcHashers[i].k <== 0;
+        left[i] <== (1 - hashDirections[i]) * rootPath[i];
+        mimcHashers[i].ins[0] <== left[i] + (hashDirections[i] * siblingNodes[i]);
+        right[i] <== (1 - hashDirections[i]) * siblingNodes[i];
+        mimcHashers[i].ins[1] <== right[i] + (hashDirections[i] * rootPath[i]);
+
+        rootPath[i+1] <== mimcHashers[i].outs[0];
+    }
+    _root === rootPath[levels];
+
+    // This is to prevent MEV attacks
+    signal addressSquared;
+    addressSquared <== _address * _address;
+}
+
+component main {public [_root, _nullifierHash, _address]} = Withdraw(10);
--- src/Tornado.sol
+++ src/Tornado.sol
@@ -24,12 +24,14 @@
 pragma solidity ^0.8.27;
 
 interface IMiMC {
-    function MiMCSponge(uint256 in_xL, uint256 in_xR) external pure returns (uint256 xL, uint256 xR);
+    function MiMCSponge(
+        uint256 in_xL,
+        uint256 in_xR
+    ) external pure returns (uint256 xL, uint256 xR);
 }
 
 contract Tornado {
     // Errors ///////////////////////////////////////////////////////////////////////////////////////
-    // Can I make this display with the contract denomination
     error Tornado__DepositAmountIsNotProperDenomination();
     error Tornado__CommitmentAlreadyHasADeposit();
     error Tornado__TreeLevelsExceedsTen();
@@ -50,20 +52,44 @@ contract Tornado {
     uint256 private constant FIELD_MODULUS =
         21888242871839275222246405745257275088548364400416034343698204186575808495617;
     bytes32[10] initialNodeValues = [
-        bytes32(0x0de70e2c8239509d2b8be8701de9657180da637f09f4063046f5f3d90b01b5d9),
-        bytes32(0x211436a028b38dcd6f02492ea42254a7fe34a03fef3baaddb357156ad84480b1),
-        bytes32(0x023e3b895367a1e60223c601d6823a42d78d199e7174c5eb3e4c29cabf7c4dd3),
-        bytes32(0x03417128982f9a3eecf227fb511ed46edae71991d68861548b1c081cf35762e3),
-        bytes32(0x122caecc4e02fcf1d4867fc71beca700706767c4337a90fdef096ca2a85cd551),
-        bytes32(0x2ecc7cab28d34610c27369cc8688581c7ddd283d62e1558121d2ae9fb06eff59),
-        bytes32(0x2f5b206c3d94be12e74877e7c2681abb29d41715083fd69a5ef410dc747bf7f0),
-        bytes32(0x0047482acf7237ec934584082a5bebcbfec5fb3a47b202de12b8e1ce9f3a38c4),
-        bytes32(0x24bf01b2dca343cd72e4d27b4e548d84799b81096aa894c7e825780ff07ea24d),
-        bytes32(0x27600a82f00c73b9b35841dbbf3c91362c7127c4016ab9deef248a9da45ba6d0)
+        bytes32(
+            0x0de70e2c8239509d2b8be8701de9657180da637f09f4063046f5f3d90b01b5d9
+        ),
+        bytes32(
+            0x211436a028b38dcd6f02492ea42254a7fe34a03fef3baaddb357156ad84480b1
+        ),
+        bytes32(
+            0x023e3b895367a1e60223c601d6823a42d78d199e7174c5eb3e4c29cabf7c4dd3
+        ),
+        bytes32(
+            0x03417128982f9a3eecf227fb511ed46edae71991d68861548b1c081cf35762e3
+        ),
+        bytes32(
+            0x122caecc4e02fcf1d4867fc71beca700706767c4337a90fdef096ca2a85cd551
+        ),
+        bytes32(
+            0x2ecc7cab28d34610c27369cc8688581c7ddd283d62e1558121d2ae9fb06eff59
+        ),
+        bytes32(
+            0x2f5b206c3d94be12e74877e7c2681abb29d41715083fd69a5ef410dc747bf7f0
+        ),
+        bytes32(
+            0x0047482acf7237ec934584082a5bebcbfec5fb3a47b202de12b8e1ce9f3a38c4
+        ),
+        bytes32(
+            0x24bf01b2dca343cd72e4d27b4e548d84799b81096aa894c7e825780ff07ea24d
+        ),
+        bytes32(
+            0x27600a82f00c73b9b35841dbbf3c91362c7127c4016ab9deef248a9da45ba6d0
+        )
     ];
 
     // Events ///////////////////////////////////////////////////////////////////////////////////////
-    event Deposit(bytes32 commitment, bytes32[] treePath, uint8[] hashDirections);
+    event Deposit(
+        bytes32 commitment,
+        bytes32[] treePath,
+        uint8[] hashDirections
+    );
 
     // Functions ////////////////////////////////////////////////////////////////////////////////////
     constructor(uint8 _levels, uint8 _denomination, IMiMC _mimc) {
@@ -90,7 +116,7 @@ contract Tornado {
         uint16 currentIndex = nextDepositIndex;
         bytes32 left;
         bytes32 right;
-        uint8[] memory hashDirections; // index of previous hash element in calculation of next hash element
+        uint8[] memory hashDirections; // index of the last hash element in calculation of next hash element
         bytes32[] memory newTreePath;
         newTreePath[0] = _commitment;
         for (uint8 i = 0; i < levels; i++) {
@@ -108,7 +134,9 @@ contract Tornado {
         lastTreePath = newTreePath;
 
         commitmentsUsed[_commitment] = true;
-        lastThirtyRoots[nextDepositIndex % NUM_OF_PREV_ROOTS] = lastTreePath[levels + 1];
+        lastThirtyRoots[nextDepositIndex % NUM_OF_PREV_ROOTS] = lastTreePath[
+            levels + 1
+        ];
         nextDepositIndex++;
         emit Deposit(_commitment, lastTreePath, hashDirections);
     }
@@ -119,7 +147,11 @@ contract Tornado {
     /**
      * @dev Hashes 2 tree nodes
      */
-    function hashLeftRight(IMiMC _hasher, bytes32 _left, bytes32 _right) public pure returns (bytes32) {
+    function hashLeftRight(
+        IMiMC _hasher,
+        bytes32 _left,
+        bytes32 _right
+    ) public pure returns (bytes32) {
         if (uint256(_left) > FIELD_MODULUS) {
             revert Tornado__HashElementNotInField();
         }
