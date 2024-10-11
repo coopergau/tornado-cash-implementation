5c0de132b55f794ad5f59d97cf3479eb57185e65
coopergau <cooperg122@gmail.com>
2024-10-11 16:42:15 -0600

    ran forge fmt, and added broadcast to gitignore

--- script/DeployTornado.s.sol
+++ script/DeployTornado.s.sol
@@ -7,21 +7,12 @@ import {Groth16Verifier} from "../src/Verifier.sol";
 import {Tornado} from "../src/Tornado.sol";
 
 contract DeployTornado is Script {
-    address public constant MIMC_CONTRACT_ADDRESS =
-        0x83584f83f26aF4eDDA9CBe8C730bc87C364b28fe; // Actual contract address that Tornado Cash uses on mainnet
+    address public constant MIMC_CONTRACT_ADDRESS = 0x83584f83f26aF4eDDA9CBe8C730bc87C364b28fe; // Actual contract address that Tornado Cash uses on mainnet
 
-    function run(
-        uint8 _levels,
-        uint256 _denomination
-    ) external returns (Tornado, Groth16Verifier) {
+    function run(uint8 _levels, uint256 _denomination) external returns (Tornado, Groth16Verifier) {
         vm.startBroadcast();
         Groth16Verifier verifier = new Groth16Verifier();
-        Tornado tornado = new Tornado(
-            _levels,
-            _denomination,
-            MIMC_CONTRACT_ADDRESS,
-            address(verifier)
-        );
+        Tornado tornado = new Tornado(_levels, _denomination, MIMC_CONTRACT_ADDRESS, address(verifier));
         vm.stopBroadcast();
 
         return (tornado, verifier);
